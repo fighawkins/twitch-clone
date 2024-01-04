d981e41b7d5b439248ad1899f6ec2c5799db5bba
Fig <fig.hawkins@pcc.edu>
2024-01-03 16:58:03 -0800

    sidebar setup

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,10 +4,8 @@ import {UserButton} from "@clerk/nextjs"
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-     <UserButton
-     afterSignOutUrl='/'
-     />
+      <h1>HomePage</h1>
+     
      </div>
 
   )
---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { Toggle } from "./toggle"
+import { Wrapper } from "./wrapper"
+
+export const Sidebar = () => {
+    return (
+        <Wrapper>
+                <Toggle />
+        </Wrapper>
+         
+    )
+}
